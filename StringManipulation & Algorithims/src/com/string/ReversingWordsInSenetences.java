145ca2a62714d66309a2ff8b38b52ccb10393985
Ankushacc <ankushsingh.gre@gmail.com>
2015-07-18 17:43:27 -0700

    Using Java

--- StringManipulation & Algorithims/src/com/string/ReversingWordsInSenetences.java
+++ StringManipulation & Algorithims/src/com/string/ReversingWordsInSenetences.java
@@ -2,24 +2,43 @@ package com.string;
 
 /**
  * pritning a string in which it will reverse the words from last for example :
- * My dream is to start a company output should be: company a start to is dream
- * my
+ * Input :  My dream is to start a company
+ * Output should be: company a start to is dream My
+ * 
  **/
 
+/**
+ * 
+ * @author Ankush :- this question could be done in several ways using other data
+ *         structure like hashmap, linked list and other. but here is solution
+ *         by string only. Complexity for this solution is : O(N) where N-
+ *         length of input string
+ * 
+ */
 public class ReversingWordsInSenetences {
 
 	public static void main(String[] agrs) {
 		StringBuffer sb = new StringBuffer();
-		StringBuffer result = new StringBuffer();
-		String in = "this is career";
-		for (int i = in.length()-1; i >= 0; i--) {
-			sb.append(in.charAt(i));
+		StringBuffer out = new StringBuffer();
+		String in = " My dream is to start a company";
+		int pos = 1;
+		sb.append(' ');
+		for (int i = in.length() - 1; i >= 0; i--) {
+			if (in.charAt(i) != ' ') {
+				sb.append(in.charAt(i));
+			}
+			if (in.charAt(i) == ' ' || i == 0) {
+				StringBuffer result = new StringBuffer();
+				for (int k = sb.length() - 1; k >= 0; k--) {
+					result.append(sb.charAt(k));
+				}
+				out = out.append(result);
+				sb = new StringBuffer();
+				sb.append(' ');
+			}
+
+			System.out.println("here is: \t" + out.toString());
 		}
-		System.out.println(">> " + sb.toString());
-		for (int i = sb.length()-1; i >= 0; i--) {
-			result.append(sb.charAt(i));
-		}
-		
 
 	}
 }
