393e7b100e6a20e2b6c0246302f5a65e46dfddc2
Ankushacc <ankushsingh.gre@gmail.com>
2015-07-21 01:23:59 -0700

    Java Implementation

---
+++ StringManipulation & Algorithims/src/com/string/MaxInHashMap.java
@@ -0,0 +1,34 @@
+package com.string;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+
+public class MaxInHashMap {
+
+	public static void main(String args[]) {
+		// create a hash set
+		String string = "";
+
+		LinkedHashSet hs = new LinkedHashSet();
+		// add elements to the hash set
+		String str = "aaaabbbccssss ";
+		int count = 1;
+		int local = 1;
+		for (int i = 0; i < str.length(); i++) {
+			if ((hs.add(str.charAt(i)))) {
+				string += local;
+				string += str.charAt(i);
+
+				count = 1;
+
+			} else {
+				count++;
+
+				local = count;
+
+			}
+
+		}
+		System.out.println(string.substring(1, string.length() - 1));
+	}
+}
